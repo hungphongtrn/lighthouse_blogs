e374ff8b75aa65208dc85083f8d6edf4e7f80e65
hungphongtrn <phonglearntodev@gmail.com>
2024-09-04 09:23:01 +0700

    update: udpate about page

--- src/components/About/AboutCoverSection.js
+++ src/components/About/AboutCoverSection.js
@@ -17,10 +17,10 @@ const AboutCoverSection = () => {
 
         <div className='w-full md:w-1/2 flex flex-col text-left items-start justify-center px-5 xs:p-10 pb-10 lg:px-16'>
             <h2 className='font-bold capitalize text-4xl xs:text-5xl sxl:text-6xl  text-center lg:text-left'>
-            Dream Big, Work Hard, Achieve More!
+              Illuminate the Unknown, Empower the Future!
             </h2>
             <p className='font-medium capitalize mt-4 text-base'>
-            This Mantra Drives My Work As A Passionate Freelancer. I Blend Innovative Technology With Timeless Design For Captivating Digital Experiences. Inspired By Nature And Literature, I'm A Perpetual Learner Embracing Challenges. With Each Project, I Aim To Leave A Lasting Impact—One Pixel At A Time.
+              At Lighthouse Research, we believe that knowledge should be as open and expansive as the horizons we seek to explore. By challenging the norms and pushing beyond closed technologies, we aim to shed light on new possibilities, guiding innovation toward a future where every breakthrough is a shared triumph.
             </p>
         </div>
     </section>
